b9ad8aa2f829f36589bcbf5de9d56c22c2bff8b0
Jonathan Weng <JWeng@MacBook-Pro-2.local>
2025-01-25 09:02:10 -0500

    blog template updates

--- scripts/blog.js
+++ scripts/blog.js
@@ -161,11 +161,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-// Load the tags-container.html file dynamically and insert its content into the placeholder (<div id="tagsContainer"></div>) in each blog page
+// Load the centralized blog-sidebar.html file dynamically and insert its content into the placeholder (<div id="blogSidebar"></div>) in each blog page
 document.addEventListener('DOMContentLoaded', () => {
-    const tagsContainer = document.getElementById('tagsContainer');
-    if (tagsContainer) {
-        fetch('/blog/tags-container.html')
+    const blogSidebar = document.getElementById('blogSidebar');
+    if (blogSidebar) {
+        fetch('/blog/blog-sidebar.html')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Failed to fetch tags.html');
@@ -173,7 +173,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 return response.text();
             })
             .then(html => {
-                tagsContainer.innerHTML = html;
+                blogSidebar.innerHTML = html;
+            })
+            .catch(error => {
+                console.error('Error loading tags:', error);
+            });
+    }
+});
+
+
+// Load the centralized author-box-jw.html file dynamically and insert its content into the placeholder (<div id="authorBoxJW"></div>) in each blog page
+document.addEventListener('DOMContentLoaded', () => {
+    const authorBoxJW = document.getElementById('authorBoxJW');
+    if (authorBoxJW) {
+        fetch('/blog/author-box-jw.html')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch tags.html');
+                }
+                return response.text();
+            })
+            .then(html => {
+                authorBoxJW.innerHTML = html;
             })
             .catch(error => {
                 console.error('Error loading tags:', error);
