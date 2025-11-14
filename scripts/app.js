4c860d2c2f04d542d686fb739bd995411a46747d
Jonathan Weng <JWeng@MacBook-Pro-2.local>
2025-11-14 02:43:30 -0500

    mood palette implementation

--- scripts/app.js
+++ scripts/app.js
@@ -33,6 +33,7 @@ window.addEventListener('DOMContentLoaded', event => {
             { name: "Tools", group: true, children: [
                 { name: "Calculators", url: "/calculators.html" },
                 { name: "File Viewer", url: "/file-viewer.html" },
+                { name: "Mood Palette", url: "/mood-palette.html" },
                 { name: "Notes App", url: "/notes.html" },
                 { name: "Price Pilot", url: "/price-pilot.html" },
                 { name: "Tier List Maker", url: "/tier-list.html" },
